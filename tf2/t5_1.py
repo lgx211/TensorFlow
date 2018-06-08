b6158ed05aa01bec8c05bb290c56741fddf1724b
lgx211 <2029666191@qq.com>
2018-06-08 15:37:12 +0800

    4

--- tf2/t5_1.py
+++ tf2/t5_1.py
@@ -46,7 +46,7 @@ y = tf.matmul(y1, w2) + b2
 
 loss_mse = tf.reduce_mean(tf.square(y - y_))
 loss_total = loss_mse + tf.add_n(tf.get_collection('losses'))
-train_step = tf.train.AdamOptimizer(0.0001).minimize(loss_mse)
+train_step = tf.train.AdamOptimizer(0.0001).minimize(loss_total)
 
 with tf.Session() as sess:
     init_op = tf.global_variables_initializer()
@@ -58,8 +58,8 @@ with tf.Session() as sess:
         end = start + BATCH_SIZE
         sess.run(train_step, feed_dict={x: X[start:end], y_: Y_[start:end]})
         if i % 2000 == 0:
-            loss_mse_v = sess.run(loss_mse, feed_dict={x: X, y_: Y_})
-            print("After %d steps ,loss is : %f  " % (i, loss_mse_v))
+            loss_v = sess.run(loss_total, feed_dict={x: X, y_: Y_})
+            print("After %d steps ,loss is : %f  " % (i, loss_v))
     xx, yy = np.mgrid[-3:3:.01, -3:3:.01]
     grid = np.c_[xx.ravel(), yy.ravel()]
     probs = sess.run(y, feed_dict={x: grid})
@@ -68,6 +68,7 @@ with tf.Session() as sess:
     print("w1", sess.run(w1))
     print("w1", sess.run(b1))
     print("w1", sess.run(w2))
+    print("w1", sess.run(b2))
 
 plt.scatter(X[:, 0], X[:, 1], c=np.squeeze(Y_c))
 plt.contour(xx, yy, probs, levels=[.5])
