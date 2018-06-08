32a232047e36e859e5ccbed3e2eb94f1b048900f
lgx211 <2029666191@qq.com>
2018-06-08 09:53:45 +0800

    2

--- tf1/t4_2.py
+++ tf1/t4_2.py
@@ -11,4 +11,9 @@ y = tf.matmul(a, w2)
 with tf.Session() as sess:
     init_op = tf.global_variables_initializer()
     print(sess.run(init_op))
-    print(sess.run(y, feed_dict={x: [[0.7, 0.5], [0.4, 0.7]]}))
+
+    print("w1是:", sess.run(w1))
+    print("w2是:", sess.run(w2))
+    print("a是:", sess.run(a, feed_dict={x: [[0.7, 0.5], [0.4, 0.7]]}))
+
+    print("y是:", sess.run(y, feed_dict={x: [[0.7, 0.5], [0.4, 0.7]]}))
