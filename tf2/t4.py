58c16e7123af7d036d37e8398308a0d3b1c43154
lgx211 <2029666191@qq.com>
2018-06-08 14:38:30 +0800

    3

--- tf2/t4.py
+++ tf2/t4.py
@@ -1,3 +1,46 @@
 import tensorflow as tf
-import numpy as np
 
+# 代码就是不断更新w的值，优化w的值。
+w1 = tf.Variable(0, dtype=tf.float32)
+
+# 计数器，轮数，不计入
+global_step = tf.Variable(0, trainable=False)
+
+# 平均移动衰减率
+MOVING_AVERAGE_DECAY = 0.99
+
+# 实例化滑动平均类
+ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+
+# 自动将所有待训练的参数汇总为列表
+ema_op = ema.apply(tf.trainable_variables())
+
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+
+    # 打印w1滑动平均值
+    print(sess.run([w1, ema.average(w1)]))
+
+    # 参数w1的值赋为1
+    sess.run(tf.assign(w1, 1))
+    sess.run(ema_op)
+    print(sess.run([w1, ema.average(w1)]))
+
+    # 跟新step为100轮和w1为10
+    sess.run(tf.assign(global_step, 100))
+    sess.run(tf.assign(w1, 10))
+    sess.run(ema_op)
+    print(sess.run([w1, ema.average(w1)]))
+
+    sess.run(ema_op)
+    print(sess.run([w1, ema.average(w1)]))
+
+    sess.run(ema_op)
+    print(sess.run([w1, ema.average(w1)]))
+
+    sess.run(ema_op)
+    print(sess.run([w1, ema.average(w1)]))
+
+    sess.run(ema_op)
+    print(sess.run([w1, ema.average(w1)]))
