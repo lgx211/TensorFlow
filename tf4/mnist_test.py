8a2d9440c4ccd395e80a1052bd28ca764b7ca30f
lgx211 <2029666191@qq.com>
2018-06-13 17:21:32 +0800

    注释

--- tf4/mnist_test.py
+++ tf4/mnist_test.py
@@ -22,6 +22,7 @@ def test(mnist):
 
         while True:
             with tf.Session() as sess:
+                # 断点续训
                 ckpt = tf.train.get_checkpoint_state(mnist_backword.MODEL_SAVE_PATH)
                 if ckpt and ckpt.model_checkpoint_path:
                     saver.restore(sess, ckpt.model_checkpoint_path)
