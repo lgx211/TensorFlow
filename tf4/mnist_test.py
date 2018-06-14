5f5a7a3692c6f30e7db4b257002b54c7edc6b18c
liguoxi <2029666191@qq.com>
2018-06-14 22:22:15 +0800

    保留的session

--- tf4/mnist_test.py
+++ tf4/mnist_test.py
@@ -15,6 +15,7 @@ def test(mnist):
 
         ema = tf.train.ExponentialMovingAverage(mnist_backword.MOVING_AVERAGE_DECAY)
         ema_restore = ema.variables_to_restore()
+
         saver = tf.train.Saver(ema_restore)
 
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
